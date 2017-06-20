4eb4d036bb4e8282e383448175eb4f52512357c2
Titansmasher <dannyjmay97@gmail.com>
2017-06-20 21:15:39 +0100

    Migrated from old solution

---
+++ TT2Bot/Models/Settings/TitanLordTimerData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TT2Bot.Models
+{
+    class TitanLordTimerData
+    {
+        public ulong MessageId { get; set; }
+        public ulong MessageChannelId { get; set; }
+    }
+}
