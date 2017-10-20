89f7509ebe0f7892117419e630d50103950d2c01
Mildan <MildanWorld@gmail.com>
2017-10-20 18:40:29 +0200

    Updated some formulas in Calculator

--- TT2Bot/Helpers/Calculator.cs
+++ TT2Bot/Helpers/Calculator.cs
@@ -6,38 +6,32 @@ namespace TT2Bot.Helpers
 {
     public static class Calculator
     {
-        private static int[] BossAttackCosts { get; } = new int[] { 0, 5, 25, 50, 75, 100, 125, 150 };
+        private static int[] BossAttackCosts { get; } = { 0, 5, 25, 50, 75, 100, 125, 150 };
 
         public static double TitanLordHp(int clanQuest)
-            //=> Math.Pow(10, 5) * (15 + 9 * (Math.Pow(Math.Min(clanQuest - 1, 650), 0.9) * Math.Pow(Math.Max(((double)clanQuest - 1) / 650, 1), 1.4)));
-            => Math.Pow(10, 5) * Math.Pow(clanQuest, Math.Pow(clanQuest, 0.031));
+            => Math.Pow(10, 5) * Math.Pow(clanQuest, Math.Pow(clanQuest, 0.028));
 
         public static double ClanBonus(int clanQuest)
-            => Math.Pow(1.1, Math.Min(clanQuest, 200)) *
-               Math.Pow(1.05, Math.Max(clanQuest - 200, 0));
+            => Math.Pow(1.0233, clanQuest) + Math.Pow(clanQuest, 1.05);
 
         public static int AttackCost(int attackNo)
             => BossAttackCosts[Math.Min(attackNo, BossAttackCosts.Length - 1)];
 
         public static int TotalAttackCost(int attacks)
-            => Enumerable.Range(0, attacks).Sum(v => AttackCost(v));
+            => Enumerable.Range(0, attacks).Sum(AttackCost);
 
         public static Percentage AdvanceStart(int clanQuest)
-            // Old formula
-            //=> Math.Min(0.5, Math.Min((double)clanQuest, 200) / 1000 + Math.Max((double)clanQuest - 200, 0) / 2000);
             => Math.Min(0.003 * Math.Pow(Math.Log(clanQuest + 4), 2.692), 0.9);
 
         public static int AttacksNeeded(int clanLevel, int attackers, int maxStage, int tapsPerAttack)
             => (int)Math.Ceiling((TitanLordHp(clanLevel) / attackers) / (Math.Max(50, maxStage) * tapsPerAttack + Math.Max(50, maxStage) * 90 * 2));
 
         public static int RelicsEarned(int stage, int bosLevel)
-            // Old formula
-            // (int)Math.Floor((1 + bosLevel * 0.05) * Math.Pow((((double)stage - 75) / 14), 1.75));
-            => (int) (Math.Max(Math.Ceiling(3.0 * Math.Pow(1.21, Math.Pow(stage, 0.48)) + 1.5 * (stage - 110)), 0)
-                * (1 + bosLevel * 0.05));
+            => (int)(Math.Max(Math.Ceiling(3.0 * Math.Pow(1.21, Math.Pow(stage, 0.48)) + 1.5 * (stage - 110)), 0)
+                     * (1 + bosLevel * 0.05));
 
         public static int TitansOnStage(int stage, int ipLevel)
-            => Math.Max(1, (stage / 1000) * 4 + 10 - ipLevel);
+            => Math.Max(1, (stage / 500) * 2 + 10 - ipLevel);
 
         public static TimeSpan RunTime(int from, int to, int ipLevel, int splashAmount)
             => new TimeSpan(0, 0, (int)Math.Ceiling((to - from) + Enumerable.Range(from, to - from).Sum(s => TitansOnStage(s, ipLevel) / splashAmount + TitansOnStage(s, ipLevel) % splashAmount) * 0.4));
