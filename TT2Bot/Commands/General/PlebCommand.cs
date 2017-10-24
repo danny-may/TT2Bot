233b185e6e248adc79384708e73ef3c21ced7df3
Titansmasher <dannyjmay97@gmail.com>
2017-10-24 21:08:42 +0100

    Nothing to see here

---
+++ TT2Bot/Commands/General/PlebCommand.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using TitanBot.Commands;
+using TitanBot.Formatting;
+
+namespace TT2Bot.Commands.General
+{
+    [Hidden]
+    [RequireGuild(271733361863557129)]
+    internal class PlebCommand : Command
+    {
+        [Call]
+        private async Task PlebAsync()
+            => await ReplyAsync(new RawString("Relajarse is a pleb >:D"));
+    }
+}
